37348c2b739b4f699ba4b893ae03619face77e29
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-29 01:48:40 +0300

    Reduced ifman, simplified get_closes_bar function

--- bars.py
+++ bars.py
@@ -5,9 +5,29 @@ import sys
 
 
 def load_data(filepath):
-    with open(filepath, encoding='cp1251') as jsonfile:
-        content = jsonfile.read()
-        return json.loads(content)
+    try:
+        with open(filepath, encoding='cp1251') as jsonfile:
+            content = jsonfile.read()
+            return json.loads(content)
+    except FileNotFoundError:
+        sys.exit("Неверный путь к файлу или имя файла")
+    except json.decoder.JSONDecodeError:
+        sys.exit("Файл не является файлов JSON")
+
+def get_coordinates():
+    longitude = None
+    latitude = None
+    while not longitude:
+        try:
+            longitude = float(input('Введите долготу: '))
+        except ValueError:
+            print('Неверные данные попробуйте еще раз')
+    while not latitude:
+        try:
+            latitude = float(input('Введите широту: '))
+        except ValueError:
+            print('Неверные данные попробуйте еще раз')
+    return longitude, latitude
 
 def get_biggest_bar(data):
     bar = max(data, key=lambda seats: seats['SeatsCount'])
@@ -19,28 +39,16 @@ def get_smallest_bar(data):
     return bar['Name'], bar['Address']
 
 
-def get_closest_bar(data, longitude, latitude):
-    coordinates = [longitude, latitude]
-    dist = lambda s, d: (s[0] - d[0]) ** 2 + (s[1] - d[1]) ** 2
+def get_closest_bar(data, coordinates):
     json_coordinates = [bar['geoData']['coordinates'] for bar in data]
-    nearest_coordinates = min(json_coordinates, key=partial(dist, coordinates))
-    bar = list(filter(lambda bar: bar['geoData']['coordinates'] == nearest_coordinates, data))[0]
+    closest_coord = min(json_coordinates, key=lambda c: (c[0]- coordinates[0])**2 + (c[1]-coordinates[1])**2)
+    bar = list(filter(lambda x: x.get('geoData').get('coordinates') == closest_coord, data))[0]
     return bar['Name'], bar['Address']
 
 if __name__ == '__main__':
-    try:
-        filepath = sys.argv[1]
-        json_data = load_data(filepath)
-
-        coordinates = input("Введите ваши координаты через запятую, вида 'долгота, широта': ")
-        while not re.match('^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$', coordinates):
-            coordinates = input("Неверный ввод. Введите ваши координаты через запятую, вида 'широта, долгота': ")
-        longitude, latitude, = [float(coordinate.strip()) for coordinate in coordinates.split(",")]
-        print("Cамый большой бар: ", get_biggest_bar(json_data))
-        print("Cамый маленький бар: ", get_smallest_bar(json_data))
-        print("Ближайший бар:", get_closest_bar(json_data, longitude, latitude))
-
-    except FileNotFoundError:
-        print("Неверный путь к файлу или имя файла")
-    except json.decoder.JSONDecodeError:
-        print("Файл не является файлов JSON")
+    file_path = sys.argv[1]
+    json_data = load_data(file_path)
+    coordinates = get_coordinates()
+    print("Cамый большой бар: ", get_biggest_bar(json_data))
+    print("Cамый маленький бар: ", get_smallest_bar(json_data))
+    print("Ближайший бар:", get_closest_bar(json_data, coordinates))
